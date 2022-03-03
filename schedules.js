73062428ec04fc2de68756fc5baf6e48644958a4
Adam Taylor <adamtaylor0795@gmail.com>
2022-03-02 21:13:12 -0600

    Remove image loading event

--- schedules.js
+++ schedules.js
@@ -8,34 +8,12 @@ var scheduleLocation =
     currentScript.attributes["data-schedule-location"].nodeValue;
 var branch = currentScript.attributes["data-branch"].nodeValue;
 var SCHEDULE_URL = `https://raw.githubusercontent.com/adamtaylor13/profectus-schedules/${branch}/dist/schedule/${scheduleLocation}_schedule.html`;
-var IMAGE_URL = `https://raw.githubusercontent.com/adamtaylor13/profectus-schedules/${branch}/dist/img/${scheduleLocation}_schedule.png`;
 var xhr = new XMLHttpRequest();
 
-var screenTooSmall = window.outerWidth <= 1000;
-if (screenTooSmall) {
-    loadScheduleImage();
-} else {
-    xhr.onreadystatechange = onScheduleCall;
-    xhr.overrideMimeType("application/javascript");
-    xhr.open("GET", SCHEDULE_URL);
-    xhr.send();
-}
-
-function loadScheduleImage() {
-    var img = document.createElement("img");
-    img.src = IMAGE_URL;
-    img.style = "width:100vw";
-    // On tap, navigate to this picture
-    img.addEventListener(
-        "click",
-        function () {
-            window.location.href = IMAGE_URL;
-        },
-        false
-    );
-    currentScript.insertAdjacentElement("afterend", img);
-    sendTestEvent();
-}
+xhr.onreadystatechange = onScheduleCall;
+xhr.overrideMimeType("application/javascript");
+xhr.open("GET", SCHEDULE_URL);
+xhr.send();
 
 function onScheduleCall(xhrResponse) {
     if (xhr.readyState === XMLHttpRequest.DONE) {
